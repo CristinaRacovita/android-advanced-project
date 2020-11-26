768ea62daf84b472661a63c4c950c9ef26285654
cristina.racovita98@gmail.com <cristina.racovita98@gmail.com>
2020-11-26 22:04:20 +0200

    splash activity

---
+++ tasty/app/src/main/java/com/example/tasty/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.tasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
