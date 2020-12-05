b91e33ace71d7f4e52c2a3a0ef0bad4941aead31
CristinaRacovita <misumisu@gmail.com>
2020-12-05 14:15:13 +0200

    main adapter

--- tasty/app/src/main/java/com/example/tasty/presentation/binderAdapters/MainBinderAdapter.java
+++ tasty/app/src/main/java/com/example/tasty/presentation/binderAdapters/MainBinderAdapter.java
@@ -3,12 +3,14 @@ package com.example.tasty.presentation.binderAdapters;
 import android.util.Log;
 import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.databinding.BindingAdapter;
 
 public class MainBinderAdapter {
 
-    @BindingAdapter({"string"})
-    public static void getItem(FrameLayout frameLayout, String string) {
-        Log.d("MAIN ACTIVITY", "main activity" + string);
+    @BindingAdapter({"bottomNav"})
+    public static void getItem(FrameLayout frameLayout, BottomNavigationView bottomNavigationView) {
+        Log.d("MAIN ACTIVITY", "main activity binder adapter works");
     }
 }
--- tasty/app/src/main/java/com/example/tasty/presentation/viewmodel/MainViewModel.java
+++ tasty/app/src/main/java/com/example/tasty/presentation/viewmodel/MainViewModel.java
@@ -6,16 +6,5 @@ import androidx.databinding.ObservableField;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    public ObservableField<Field> menuSelectedOption = new ObservableField<>();
 
-    public ObservableField<Field> getMenuSelectedOption() {
-        return menuSelectedOption;
-    }
-
-    public void setMenuSelectedOption(ObservableField<Field> menuSelectedOption) {
-        this.menuSelectedOption = menuSelectedOption;
-    }
-
-    public MainViewModel() {
-    }
 }
