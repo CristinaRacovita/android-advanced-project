d0d6f2d6ccd1335c5bfd358722aeab467a74231f
CristinaRacovita <misumisu@gmail.com>
2020-12-05 14:16:44 +0200

    main adapter

--- tasty/app/src/main/java/com/example/tasty/presentation/binderAdapters/MainBinderAdapter.java
+++ tasty/app/src/main/java/com/example/tasty/presentation/binderAdapters/MainBinderAdapter.java
@@ -3,14 +3,12 @@ package com.example.tasty.presentation.binderAdapters;
 import android.util.Log;
 import android.widget.FrameLayout;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.databinding.BindingAdapter;
 
 public class MainBinderAdapter {
 
-    @BindingAdapter({"bottomNav"})
-    public static void getItem(FrameLayout frameLayout, BottomNavigationView bottomNavigationView) {
+    @BindingAdapter("get")
+    public static void getItem(FrameLayout frameLayout) {
         Log.d("MAIN ACTIVITY", "main activity binder adapter works");
     }
 }
