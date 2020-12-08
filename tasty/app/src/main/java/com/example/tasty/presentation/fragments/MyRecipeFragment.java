0da65feb5046489723667e28ef2951680898a955
cristina.racovita98@gmail.com <cristina.racovita98@gmail.com>
2020-12-08 16:42:43 +0200

    my recipe fragment created

---
+++ tasty/app/src/main/java/com/example/tasty/presentation/fragments/MyRecipeFragment.java
@@ -0,0 +1,28 @@
+package com.example.tasty.presentation.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tasty.R;
+
+public class MyRecipeFragment extends Fragment {
+
+    public static MyRecipeFragment newInstance(){
+        return new MyRecipeFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_recipe, container, false);
+    }
+}
