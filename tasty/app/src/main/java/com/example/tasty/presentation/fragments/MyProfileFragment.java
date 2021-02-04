fa27a9195b65b759546f2400dbcb454db28bde8f
cristina.racovita98@gmail.com <cristina.racovita98@gmail.com>
2021-02-04 15:24:13 +0200

    menu profile implemented

--- tasty/app/src/main/java/com/example/tasty/presentation/fragments/MyProfileFragment.java
+++ tasty/app/src/main/java/com/example/tasty/presentation/fragments/MyProfileFragment.java
@@ -1,13 +1,19 @@
 package com.example.tasty.presentation.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.tasty.R;
+import com.example.tasty.presentation.activities.SettingsActivity;
 
 public class MyProfileFragment extends Fragment {
 
@@ -22,6 +28,22 @@ public class MyProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_my_profile, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_profile, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            getActivity().startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
