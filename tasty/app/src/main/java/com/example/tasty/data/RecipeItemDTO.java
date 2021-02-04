f33874edbaf495538226fc1884fe8122024028fe
cristina.racovita98@gmail.com <cristina.racovita98@gmail.com>
2021-02-04 14:25:24 +0200

    conflicts resolved

--- tasty/app/src/main/java/com/example/tasty/data/RecipeItemDTO.java
+++ tasty/app/src/main/java/com/example/tasty/data/RecipeItemDTO.java
@@ -3,13 +3,8 @@ package com.example.tasty.data;
 import com.google.gson.annotations.SerializedName;
 
 public class RecipeItemDTO {
-<<<<<<< HEAD
-    @SerializedName("title")
-    private final String titleRecipe;
-=======
     @SerializedName("recipe_id")
     private final String recipeTitleId;
->>>>>>> new_language
 
     @SerializedName("image_url")
     private final String imageUrl;
